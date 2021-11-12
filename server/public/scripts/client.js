63a43a1bbf1020879e4733f399bd0c562b84c711
AllenJoeG <allen.joeg@gmail.com>
2021-11-12 09:00:56 -0600

    initial POST route for /calculate set up. Currently doesn't send anything

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,3 +15,23 @@ function numberClicked(){
 function clearButton(){
   $('#calcDisplayOut').empty();
 };
+
+function submitCalculation() {
+  //Create array that holds the calculation instructions to be sent as data via POST
+  let calculation = [];
+  // store 'numbers', 'operator', 'numbers', 'operator', 'numbers'
+  //so we can pull the array apart to make our math calc server side
+
+  //AJAX POST the to-be-calculated to /calculate POST route on server.js
+  $.ajax({
+    method: 'POST',
+    url: '/calculate',
+    data: calculation,
+  }).then((response) => {
+    console.log('Calculation Array SENT!')
+    
+  }).catch((error) => {
+    console.log('Calculation Array did NOT send')
+
+  })
+};//End Function
--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ app.use(express.json());
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+const calculationsArray = [];
+
+//POST Route receiving to-be-calculated from client
+app.post('/calculate', (req, res) => {
+  calculationsArray.push(req.body);
+  console.log(calculationsArray);
+  
+  res.sendStatus(201);
+});
+
+
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
